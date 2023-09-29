c008adf8ea01567421713a6b8603ce37b69746b1
ZendataTest <146233579+ZendataTest@users.noreply.github.com>
2023-09-29 09:19:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-account = example@gmail.com
-password = 12345test
+
+account = "example@gmail.com"
+password = "12345test"
 password1 = "P@ssw0rd123"
 password2 = "Secur3P@ss"
 password3 = "MyS3cr3tP@ss!"
 
-secrets = token
-token = 12343test
+secrets = "token"
+
+token = "12343test"
